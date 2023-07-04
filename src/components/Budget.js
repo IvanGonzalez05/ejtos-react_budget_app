b4b97d16d57b8ebf0d26e5537e6afe0e5a21753c
Ivan Gonzalez <igonzalezhansen@gmail.com>
2023-07-04 15:43:06 -0400

    Budget can be changed

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,11 +2,41 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    // const { budget, dispatch, expenses } = useContext(AppContext);
+    const { budget, dispatch } = useContext(AppContext);
+    const updateBudget = (event) => {
+        const value = event.target.value;
+        // if(value > 20000) {
+        //     alert('The value cannot exceed remaining funds');
+        //     dispatch({
+        //         type: 'SET_BUDGET',
+        //         payload: budget,
+        //     });
+        //     return;
+        // }
+        // const spending = expenses.reduce((acc, curr) => acc + curr.cost, 0)
+        // if (value < spending) {
+        //     alert('The value cannot be lower than current spending');
+        //     dispatch({
+        //         type: 'SET_BUDGET',
+        //         payload: budget,
+        //     });
+        //     return;
+        // }
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: value,
+        });
+    };
 
     return (
         <div className='alert alert-secondary'>
             <span>Budget: £{budget}</span>
+            <input
+                type='number'
+                step='10'
+                onChange={updateBudget}
+            />
         </div>
     );
 };
