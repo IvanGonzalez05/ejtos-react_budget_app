3a35c0a303ef8102072f58e38bb121c4b603a0c1
Ivan Gonzalez <igonzalezhansen@gmail.com>
2023-07-04 15:42:36 -0400

    Fix a typo

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const AllocationForm = (props) => {
-    const { dispatch, remaining } = useContext(AppContext);
+    const { dispatch, remaining, currency } = useContext(AppContext);
     const [name, setName] = useState('');
     const [cost, setCost] = useState('');
     const [action, setAction] = useState('');
@@ -41,7 +41,7 @@ const AllocationForm = (props) => {
                         <option value='Marketing' name='marketing'>Marketing</option>
                         <option value='Sales' name='sales'>Sales</option>
                         <option value='Finance' name='finance'>Finance</option>
-                        <option value='HR' name='hr'>HR</option>
+                        <option value='Human Resource' name='hr'>HR</option>
                         <option value='IT' name='it'>IT</option>
                         <option value='Admin' name='admin'>Admin</option>
                     </select>
@@ -54,14 +54,16 @@ const AllocationForm = (props) => {
                         <option value='Reduce' name='Reduce'>Reduce</option>
                     </select>
 
-                    <input
-                        required='required'
-                        type='number'
-                        id='cost'
-                        value={cost}
-                        style={{marginLeft: '2rem', size:10 }}
-                        onChange={(event) => setCost(event.target.value)}
-                    ></input>
+                    <span style={{marginLeft: '2rem', size:10 }}>
+                        {currency}
+                        <input
+                            required='required'
+                            type='number'
+                            id='cost'
+                            value={cost}
+                            onChange={(event) => setCost(event.target.value)}
+                        ></input>
+                    </span>
 
                     <button className='btn btn-primary' onClick={submitEvent} style={{ marginLeft: '2rem' }}>Save</button>
                 </div>
